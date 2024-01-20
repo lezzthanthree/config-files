0951b9e6209ac54bae98443bc6cfd42a32e3da92
lezzthanthree <arisatominato48@gmail.com>
2024-01-20 17:32:03 +0800

    Added nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,2 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
