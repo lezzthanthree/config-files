a245ccc7da22e449451e75e1a7648c6010f47271
lezzthanthree <arisatominato48@gmail.com>
2024-01-21 10:27:00 +0800

    Disable Autocomplete

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,7 +1,9 @@
 Import-Module Terminal-Icons
+Set-PSReadLineOption -PredictionSource None
 
 Set-Alias -Name lg -Value lazygit
 Set-Alias -Name touch -Value New-Item
 Set-Alias -Name rn -Value Rename-Item
 Set-Alias -Name vsc -Value code
 Set-Alias -Name ll -Value dir
+Set-Alias -Name vim -Value nvim
