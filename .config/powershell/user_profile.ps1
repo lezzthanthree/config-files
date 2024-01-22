e511df1f61cb725ab27f3bcf1e56de43bf816b4c
lezzthanthree <arisatominato48@gmail.com>
2024-01-22 23:41:42 +0800

    Alias on exit

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,5 +1,6 @@
 Import-Module Terminal-Icons
 Set-PSReadLineOption -PredictionSource None
+function ex { exit }
 
 Set-Alias -Name lg -Value lazygit
 Set-Alias -Name touch -Value New-Item
@@ -7,3 +8,5 @@ Set-Alias -Name rn -Value Rename-Item
 Set-Alias -Name vsc -Value code
 Set-Alias -Name ll -Value dir
 Set-Alias -Name vim -Value nvim
+Set-Alias -Name qq -Value ex
+Set-Alias -Name q -Value ex
